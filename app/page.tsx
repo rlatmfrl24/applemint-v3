d507be10e57f6f8dd6bd7f21cd0b04a5e4269526
SulfurBottom <397love@gmail.com>
2024-07-09 13:54:04 +0900

    chore: Update dependencies and remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default async function Index() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full container flex justify-between items-center p-3 text-sm">
+        <div className="w-full container flex justify-between items-center p-3 text-sm ">
           <AuthButton />
         </div>
       </nav>
