b8add06be994248afc99a62bcf5d8bb4755c11f1
Sulfurbottom <397love@gmail.com>
2025-02-24 10:39:29 +0900

    미디어 스레드 관련 코드 정리 및 탭 구성 수정

--- app/main/new-threads/list-normal.tsx
+++ app/main/new-threads/list-normal.tsx
@@ -20,7 +20,7 @@ export const NormalThreads = () => {
 			const { data, error } = await supabase
 				.from("new-threads")
 				.select()
-				.or("type.eq.normal,type.eq.fmkorea,type.eq.battlepage")
+				.or("type.eq.normal,type.eq.fmkorea,type.eq.battlepage,type.eq.media")
 				.order("created_at", { ascending: false })
 				.order("id", { ascending: false });
 
@@ -52,6 +52,15 @@ export const NormalThreads = () => {
 							<span className="font-bold text-2xl md:text-5xl">
 								{
 									normalThreads?.filter((thread) => thread.type === "fmkorea")
+										.length
+								}
+							</span>
+						</div>
+						<div className="flex-1 text-center">
+							<h5>Media</h5>
+							<span className="font-bold text-2xl md:text-5xl">
+								{
+									normalThreads?.filter((thread) => thread.type === "media")
 										.length
 								}
 							</span>
