2598f0464016a0ef727b97cc4115df06fa255920
Sulfurbottom <397love@gmail.com>
2025-02-25 14:11:08 +0900

    NoDataBox 컴포넌트를 추가하여 데이터가 없을 때 표시 로직 구현

--- app/main/quick/page.tsx
+++ app/main/quick/page.tsx
@@ -2,56 +2,58 @@
 
 import { createClient } from "@/utils/supabase/client";
 import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
+	QueryClient,
+	QueryClientProvider,
+	useQuery,
 } from "@tanstack/react-query";
 import { AnimatePresence } from "framer-motion";
 import { DefaultThreadItem } from "../thread-item";
 import { ThreadLoading } from "../thread-loading";
+import NoDataBox from "../no-data";
 
 export default function QuickPage() {
-  const queryClient = new QueryClient();
+	const queryClient = new QueryClient();
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <QuickThread />
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<QuickThread />
+		</QueryClientProvider>
+	);
 }
 
 const QuickThread = () => {
-  const supabase = createClient();
+	const supabase = createClient();
 
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["quick-save"],
-    queryFn: async () => {
-      const { data, error } = await supabase
-        .from("quick-save")
-        .select()
-        .order("created_at", { ascending: false })
-        .order("id", { ascending: false });
+	const { data, isLoading, error } = useQuery({
+		queryKey: ["quick-save"],
+		queryFn: async () => {
+			const { data, error } = await supabase
+				.from("quick-save")
+				.select()
+				.order("created_at", { ascending: false })
+				.order("id", { ascending: false });
 
-      if (error) {
-        throw new Error(error.message);
-      }
+			if (error) {
+				throw new Error(error.message);
+			}
 
-      return data;
-    },
-  });
+			return data;
+		},
+	});
 
-  return (
-    <div className="flex flex-col gap-2 w-full">
-      {isLoading && <ThreadLoading />}
-      <AnimatePresence>
-        {data?.map((thread) => (
-          <DefaultThreadItem
-            key={thread.id}
-            thread={thread}
-            threadName="quick-save"
-          />
-        ))}
-      </AnimatePresence>
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-2 w-full">
+			{isLoading && <ThreadLoading />}
+			<AnimatePresence>
+				{data && !data.length && <NoDataBox />}
+				{data?.map((thread) => (
+					<DefaultThreadItem
+						key={thread.id}
+						thread={thread}
+						threadName="quick-save"
+					/>
+				))}
+			</AnimatePresence>
+		</div>
+	);
 };
