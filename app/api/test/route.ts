e896375354e9fc52460489283bf426c52c85f6f3
Sulfurbottom <397love@gmail.com>
2025-02-25 10:15:31 +0900

    Crawl API 개선: 필터링 및 중복 제거 로직 추가

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,6 +1,7 @@
 import { Agent, fetch } from "undici";
 import * as cheerio from "cheerio";
 import { createClient } from "@/utils/supabase/server";
+import type { CrawlItemType } from "@/lib/typeDefs";
 
 export async function GET(request: Request) {
   // The `/api` route is required for the server-side auth flow implemented
@@ -21,9 +22,48 @@ export async function GET(request: Request) {
     });
   }
 
-  return new Response("Hello, world!", {
-    headers: {
-      "content-type": "text/plain",
-    },
+  const target = "insagirl";
+  const response = await fetch(
+    `https://applemint-v3.vercel.app/api/crawl?target=${target}`,
+  );
+  const json = await response.json();
+  const rawList = json as CrawlItemType[];
+
+  const {
+    data: filterKeywordList,
+    error,
+  } = await supabase.from("filter-keyword").select("*");
+
+  console.log("🚀 ~ GET ~ filterKeywordList:", filterKeywordList);
+
+  const filtered = rawList.filter((item) => {
+    return !filterKeywordList
+      ?.filter((keyword: { method: string }) => keyword.method === "ignore")
+      .some((ignore: { value: string }) => item.url.includes(ignore.value));
   });
+
+  const {
+    data: historyData,
+    error: historyError,
+  } = await supabase.from("crawl-history")
+    .select("*")
+    .order("created_at", { ascending: false })
+    .eq("crawl_source", target);
+
+  const newData = filtered.filter((item) => {
+    return !historyData?.some((historyItem) => historyItem.url === item.url);
+  });
+
+  return new Response(
+    JSON.stringify({
+      length: newData?.length,
+      // filtered,
+      newData,
+    }),
+    {
+      headers: {
+        "content-type": "application/json",
+      },
+    },
+  );
 }
